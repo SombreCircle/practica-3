56417caa4105deac48521b91929e0b1c477ffd88
eduardo <eduardo-so-@hotmail.com>
2018-05-12 13:18:26 -0500

    lalo

---
+++ App.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react';
+import {AppRegistry,  StyleSheet, Text, View,TextInput } from 'react-native';
+
+/*export default class App extends React.Component {
+  render() {
+    return (
+        <View style={{
+          flex: 1,
+          flexDirection:'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}/>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
+
+
+export default class PizzaTranslator extends Component {
+  constructor (props) {
+    super(props);
+    this.state={text: ''}
+
+  }
+
+  render() {
+    return(
+      <View style={{padding:10}}>
+       <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding:10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '●ω●').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
